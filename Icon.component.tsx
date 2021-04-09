54576bcd42e49e9b33d7d22e59974ae3e3779114
Jonjoe Whitfield <me@jonjoe.io>
2021-04-09 13:23:49 +0100

    chore: update icon component to make it cleaner and easier

--- Icon.component.tsx
+++ Icon.component.tsx
@@ -1,47 +1,89 @@
-import * as React from "react";
+import * as React from 'react';
 
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { library, IconProp } from "@fortawesome/fontawesome-svg-core";
-import { fad } from "@fortawesome/pro-duotone-svg-icons";
-import { fal } from "@fortawesome/pro-light-svg-icons";
-import { fas } from "@fortawesome/pro-solid-svg-icons";
-import { far } from "@fortawesome/free-regular-svg-icons";
-import { fab } from "@fortawesome/free-brands-svg-icons";
+// IMPORT IF REACT NATIVE
+// import {
+//   FontAwesomeIcon,
+//   FontAwesomeIconStyle,
+// } from '@fortawesome/react-native-fontawesome';
 
-import { IconName } from "./";
+// IMPORT IF REACT
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+import {library, IconProp} from '@fortawesome/fontawesome-svg-core';
+import {fad} from '@fortawesome/pro-duotone-svg-icons';
+import {fal} from '@fortawesome/pro-light-svg-icons';
+import {fas} from '@fortawesome/pro-solid-svg-icons';
+import {far} from '@fortawesome/free-regular-svg-icons';
+import {fab} from '@fortawesome/free-brands-svg-icons';
+
+import {IconSizes} from 'app/theme';
 
 library.add(fal, fab, fad, fas, far);
 
 export type IconProps = {
   name: IconName;
-  className?: string;
+  style?: FontAwesomeIconStyle;
+  color?: string;
+  size: 's' | 'm' | 'l';
+
+  // IF REACT
+  // onClick?: () => void;
+
+  // IF REACT NATIVE
+  // onPress?: () => void;
 };
 
+export type IconName =
+  | 'home'
+  | 'circle'
+  | 'cross'
+  | 'search';
+
 const Icon: React.FC<IconProps> = (props): JSX.Element => {
-  const { name, className } = props;
+  const {name, color, size, style} = props;
 
   const iconName = mapNamePropToFaNames(name);
+  const iconSize = mapSizeToIconSize(size);
 
   return (
-    <div className={className}>
-      <FontAwesomeIcon icon={iconName} />
-    </div>
+    <FontAwesomeIcon
+      icon={iconName}
+      color={color}
+      size={iconSize}
+      style={style}
+    />
   );
 };
 
+function mapSizeToIconSize(size: IconProps['size']): number {
+  switch (size) {
+    case 's':
+      return IconSizes.S;
+    case 'm':
+      return IconSizes.M;
+    case 'l':
+      return IconSizes.L;
+    default:
+      return IconSizes.S;
+  }
+}
+
 function mapNamePropToFaNames(iconName: IconName): IconProp {
   switch (iconName) {
-    case "question-circle":
-      return ["fal", "question-circle"];
+    case 'home':
+      return ['fal', 'home'];
+  
+    case 'cross':
+      return ['fal', 'times'];
 
-    case "twitter":
-      return ["fab", "twitter"];
+    case 'search':
+      return ['fal', 'search'];
 
-    case "copyright":
-      return ["fal", "copyright"];
+    case 'circle':
+      return ['fal', 'circle'];
 
     default:
-      return ["fal", "question-circle"];
+      return ['fal', 'question-circle'];
   }
 }
 
--- index.ts
+++
@@ -1,5 +0,0 @@
-import Icon from "./Icon.component";
-
-export type IconName = "question-circle" | "copyright" | "twitter";
-
-export default Icon;
